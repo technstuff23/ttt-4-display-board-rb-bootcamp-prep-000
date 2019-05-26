9562b39782ab04794901c63e180fd7c1758fce3a
K Branch <dancingal212@gmail.com>
2019-05-26 01:48:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,3 @@
-# Define display_board that accepts a board and prints
 def display_board(val)
   puts " #{val[0]} | #{val[1]} | #{val[3]} "
   puts "-----------"
@@ -6,7 +5,6 @@ def display_board(val)
   puts "-----------"
   puts " #{val[7]} | #{val[8]} | #{val[9]} "
 end
-board = [" "," "," "," "," "," "," "," "," "]
+board = ["X","X","X"," "," "," "," "," "," "]
 
 display_board(board)
-
