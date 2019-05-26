84490925b1ffe0ce6cbccf7937f209faf5fa9876
K Branch <dancingal212@gmail.com>
2019-05-26 02:05:51 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 def display_board(val)
-  puts " #{val[0]} | #{val[1]} | #{val[3]} "
+  puts " #{val[0]} | #{val[1]} | #{val[2]} "
   puts "-----------"
-  puts " #{val[4]} | #{val[5]} | #{val[6]} "
+  puts " #{val[3]} | #{val[4]} | #{val[5]} "
   puts "-----------"
-  puts " #{val[7]} | #{val[8]} | #{val[9]} "
+  puts " #{val[6]} | #{val[7]} | #{val[8]} "
 end
-board = ["X","X","X"," "," "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
 
 display_board(board)
