66210878526bc3ccab855d97a4039dabbb9f44dd
K Branch <dancingal212@gmail.com>
2019-05-26 01:38:59 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,12 @@
 # Define display_board that accepts a board and prints
-# out the current state.
 def display_board(val)
-  puts "#{val[0]}|#{val[1]}|#{val[3]}"
+  puts " #{val[0]} | #{val[1]} | #{val[3]} "
   puts "-----------"
-  puts "#{val[4]}|#{val[5]}|#{val[6]}"
+  puts " #{val[4]} | #{val[5]} | #{val[6]} "
   puts "-----------"
-  puts "#{val[7]}|#{val[8]}|#{val[9]}"
+  puts " #{val[7]} | #{val[8]} | #{val[9]} "
 end
 board = [" "," "," "," "," "," "," "," "," "]
+
 display_board(board)
 
